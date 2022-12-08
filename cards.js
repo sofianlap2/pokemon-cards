10182d8ed888f4afc984f71907573f9a89dcb8a6
sofiane nabli <sofienne.nabli@aleia.io>
2022-12-08 20:51:09 +0100

    commit

---
+++ cards.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    var pack = document.getElementById("pokemon-pack");
+    pack.addEventListener("click", openPack);
+}
+
+function randomNumber(min, max) { 
+    return Math.ceil(Math.random() * (max - min) + min);
+} 
+
+function openPack() {
+    let cardsOpened = document.getElementById("pokemon-cards-opened");
+    while (cardsOpened.firstChild) {
+        cardsOpened.firstChild.remove()
+    }
+
+    for (let i = 0; i < 11; i++) {
+        let cardDiv = document.createElement("div");
+        cardDiv.classList.add("pokemon-card");
+
+        let cardImg = document.createElement("img");
+        cardImg.id = i;
+
+        let num = 1;
+        if (i == 10) {
+            num = randomNumber(1, 16);
+        } else {
+            num = randomNumber(17, 101); 
+        }
+
+        cardImg.src = "./pokemon-cards/base set (" + num.toString() + ").jpg";
+
+        cardDiv.appendChild(cardImg);
+
+        document.getElementById("pokemon-cards-opened").appendChild(cardDiv);
+    }
+}
